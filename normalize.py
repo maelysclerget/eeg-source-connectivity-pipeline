e0a9a3cab3defb3c025e5e65a3d0fc9c16a6de4a
Maelys Clerget <maelys.clerget@epfl.ch>
2026-07-23 11:24:41 +0200

    Adapt script to rename file

--- normalize.py
+++ normalize.py
@@ -11,6 +11,13 @@ old_endings = [
 
 new_ending = "_eeg_not_interpolated_final_preprocessed_eeg.fif"
 
+old_interpolated_endings = [
+    "_eeg_final_preprocessed_raw_sensor_level.fif",
+    "_eeg_final_preprocessed_eeg_interpolated.fif",
+]
+
+new_interpolated_ending = "_eeg_interpolated_final_preprocessed_eeg.fif"
+
 
 for subject_dir in sorted(derivatives_dir.glob("sub-*")):
     for session_dir in sorted(subject_dir.glob("ses-*")):
@@ -26,6 +33,14 @@ for subject_dir in sorted(derivatives_dir.glob("sub-*")):
             for old_ending in old_endings:
                 old_file = task_dir / f"{stem}{old_ending}"
                 new_file = task_dir / f"{stem}{new_ending}"
+
+                if old_file.exists() and not new_file.exists():
+                    old_file.rename(new_file)
+                    print(f"{old_file} -> {new_file}")
+
+            for old_ending in old_interpolated_endings:
+                old_file = task_dir / f"{stem}{old_ending}"
+                new_file = task_dir / f"{stem}{new_interpolated_ending}"
 
                 if old_file.exists() and not new_file.exists():
                     old_file.rename(new_file)
